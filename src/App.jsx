ab577a6d75a882ce5b8e96ab86e17ecbdb6aeb23
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-15 15:51:47 +0530

    initial part done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import ToDoApp from './components/to-do-app'
+
+function App() {
+  return (
+    <>
+  <ToDoApp />
+    </>
+  )
+}
+
+export default App
